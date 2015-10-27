136b1ca3a3c1b3d91bd867ed77f33d29e6cde524
Paramecium13 <paramecium13@gmail.com>
2015-10-27 11:21:18 -0400

    Added 'public static List<T> Substitute<T>(this List<T> IEnum, T toReplace, T replacement)    where T : IEquatable<T>'

---
+++ ParameciumTools/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameciumTools
+{
+	/// <summary>
+	/// Contains extension methods for .NET generic collections.
+	/// </summary>
+    public static class Extensions
+    {
+
+		public static List<T> Substitute<T>(this List<T> IEnum, T toReplace, T replacement)
+			where T : IEquatable<T>
+		{
+			List<T> NewIEnum = new List<T>(IEnum.Count());
+			foreach (var item in IEnum)
+			{
+				NewIEnum.Add(item.Equals(toReplace) ? replacement : item);
+			}
+			return NewIEnum;
+		}
+
+	}
+}
